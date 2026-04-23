5f65f117d62a4b416e03fab0e43b3b07b035dc4d
asennies21 <asenn@ies21.edu.ar>
2026-04-23 19:27:41 -0300

    express 1

---
+++ clase-20260423-express-01/index.mjs
@@ -0,0 +1,47 @@
+import express from 'express'
+
+const PUERTO = 5000
+
+
+// Instancia de servidor express
+const app = express()
+
+const obtenerCosas = (req, res)=>{
+    res.set('content-type','text/html')
+    res.status(200)
+    res.end('<h1>Hola express</h1>')
+}
+
+app.get('/',obtenerCosas)
+
+app.get('/saludo',(req, res)=>{
+    
+    res.end('Estoy en verbo POST y ruta /')
+})
+
+
+// Post
+app.post('/', (req, res)=>{
+    res.end('Estoy en verbo POST y ruta /')
+})
+
+app.listen(PUERTO, ()=>{
+    console.log(`http://localhost:${PUERTO}`)
+})
+
+
+
+///--------------------
+
+
+// const y = ()=>{
+//     console.log('hola')
+// }
+
+// const x = (_y)=>{
+//     console.log('ejecucion 1')
+//     _y()
+//     console.log('ejecucion 2 ')
+// }
+
+// x(y)
