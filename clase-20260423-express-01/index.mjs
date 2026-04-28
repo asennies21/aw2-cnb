58bf1d6ee0cc6880e492c12cbd825b1f3fa1b502
asennies21 <asenn@ies21.edu.ar>
2026-04-27 22:23:52 -0300

    Express 2

--- clase-20260423-express-01/index.mjs
+++ clase-20260423-express-01/index.mjs
@@ -1,6 +1,6 @@
 import express from 'express'
 
-const PUERTO = 5000
+const PUERTO = 3000
 
 
 // Instancia de servidor express
---
+++ clase-20260427-express-02/index.mjs
@@ -0,0 +1,43 @@
+import express from 'express'
+
+const PUERTO = 3000
+
+const app = express()
+
+// Middleware
+function middleware1(req, res, next){
+    console.log('se ejecutó el middleware 1')
+    if(true){
+        res.send('terminado en middleware 1')
+    }else{
+        next()
+    }
+}
+
+app.use(express.static('front'))
+
+app.use('/saludo', middleware1)
+
+app.get('/', (req, res)=>{
+
+    console.log('peticion')
+    res.send('Bienvenidos')
+})
+
+app.get('/saludo', (req, res)=>{
+
+    console.log('peticion')
+    res.send('Bienvenidos')
+})
+
+
+app.get('/saludo/dia', (req, res)=>{
+
+    console.log('peticion')
+    res.send('Bienvenidos')
+})
+
+
+app.listen(PUERTO, ()=>{
+    console.log(`http://localhost:${PUERTO}`)
+})
