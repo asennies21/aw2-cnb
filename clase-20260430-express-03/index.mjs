2d56ed53ea677f14da25bf9268ff741032fb4b4a
asennies21 <asenn@ies21.edu.ar>
2026-04-30 19:33:58 -0300

    Uso de parametros en express

---
+++ clase-20260430-express-03/index.mjs
@@ -0,0 +1,60 @@
+import express from 'express'
+
+const PUERTO = 3000
+
+const app = express()
+
+const productos = [
+    {
+        id: 1,
+        nombre:'pantalon',
+        precio: 50000
+    },
+    {
+        id: 2,
+        nombre:'remera',
+        precio: 15000
+    }
+]
+
+
+// Parametros de ruta
+
+app.get('/', (req, res)=>{
+
+    res.json({mensaje:"Bienvenido"})
+})
+
+app.get('/productos', (req, res)=>{
+
+    res.json(productos)
+})
+app.get('/productos/:id', (req, res)=>{
+    // parseInt
+    // Number
+    const id = Number(req.params.id) //<----- viene como una cadena
+
+    const productosFiltrados = productos.filter((producto)=>{
+        return producto.id === id
+    })
+
+    res.json(productosFiltrados)
+})
+app.get('/productos-descuento/:descuento', (req, res)=>{
+
+    const descuento = Number(req.params.descuento)
+    console.log(descuento)
+
+    const productosConDescuento = productos.map((producto)=>{
+        const calculo = producto.precio * (descuento / 100)
+        return {
+            ///...productos, //< asignacion spread
+            id: producto.id,
+            nombre: producto.nombre,
+            precio: producto.precio - calculo
+
+        }
+    })
+    res.json(productosConDescuento)
+})
+app.listen(PUERTO)
